5a4c2d189af3e988e563fb64871b4c6e5aa164e2
Rekman <rekman.heln@gmail.com>
2023-06-07 22:01:24 +0300

    Fast region request

---
+++ RegionExtension/Commands/SubCommands/FastRegionRequestSubCommand.cs
@@ -0,0 +1,61 @@
+﻿using RegionExtension.Commands.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI.DB;
+using TShockAPI;
+
+namespace RegionExtension.Commands.SubCommands
+{
+    public class FastRegionRequestSubCommand : SubCommand
+    {
+        public override string[] Names => new[] { "fastregion", "fr" };
+        public override string Description => "Create new region with two given point and params. Also send request.";
+
+        public override void InitializeParams()
+        {
+            _params = new ICommandParam[]
+            {
+                new StringParam("region", "name of new region.")
+            };
+        }
+
+        public override void Execute(CommandArgsExtension args)
+        {
+            var regionname = (string)Params[0].Value;
+            if (!TryAutoComplete((string)Params[0].Value, args, out regionname))
+            {
+                args.Player.SendErrorMessage("Region '{0}' already exist!".SFormat(regionname));
+                return;
+            }
+            var username = (UserAccount)Params[1].Value;
+            var z = ((IntParam)Params[2]).TValue;
+            var protect = ((BoolParam)Params[3]).TValue;
+            CreateFastRegionRequest(args, regionname, username, z, protect);
+        }
+
+        private void CreateFastRegionRequest(CommandArgsExtension args, string regionName, UserAccount username, int z, bool protect)
+        {
+            args.Plugin.FastRegions.Add(new FastRegion(args.Player, regionName, username.Name, z, protect, true));
+        }
+
+        public bool TryAutoComplete(string str, CommandArgsExtension args, out string result)
+        {
+            if (!Plugin.Config.AutoCompleteSameName)
+            {
+                result = str;
+                return TShock.Regions.GetRegionByName(str) != null;
+            }
+            int num = 0;
+            result = str;
+            while (TShock.Regions.GetRegionByName(result) != null)
+            {
+                result = Plugin.Config.AutoCompleteSameNameFormat.SFormat(str, num);
+                num++;
+            }
+            return true;
+        }
+    }
+}
--- RegionExtension/FastRegion.cs
+++ RegionExtension/FastRegion.cs
@@ -7,7 +7,6 @@ namespace RegionExtension
 {
     public class FastRegion
     {
-        
         private int X1, Y1, X2, Y2;
         private int point;
         private readonly TSPlayer _player;
@@ -15,16 +14,18 @@ namespace RegionExtension
         private readonly string _ownerName;
         private readonly int _z;
         private readonly bool _protect;
+        private readonly bool _isRequest;
 
         public UserAccount User { get { return _player.Account; } }
 
-        public FastRegion(TSPlayer player, string regionName, string ownerName, int z = 0, bool protect = true)
+        public FastRegion(TSPlayer player, string regionName, string ownerName, int z = 0, bool protect = true, bool isRequest = false)
         {
             _player = player;
             _regionName = regionName;
             _ownerName = ownerName;
             _z = z;
             _protect = protect;
+            _isRequest = isRequest;
             _player.SendInfoMessage("Hit tile to Set Points, or use The Grand Design.");
         }
 
@@ -75,12 +76,27 @@ namespace RegionExtension
                 Z = _z,
                 DisableBuild = _protect
             };
-
-            if(Plugin.RegionExtensionManager.DefineRegion(_player, region))
+            if (_isRequest)
+            {
+                var checkResult = Utils.CheckConfigConditions(_player, region);
+                if (!checkResult.res)
+                {
+                    _player.SendErrorMessage(checkResult.msg);
+                    return;
+                }
+                if (Plugin.RegionExtensionManager.CreateRequest(region, _player))
+                {
+                    _player.SendSuccessMessage("Region '{0}' defined!".SFormat(region.Name));
+                    _player.SendSuccessMessage("Request created!".SFormat(region.Name));
+                }
+                else
+                    _player.SendErrorMessage("Failed define region '{0}'!".SFormat(region.Name));
+                return;
+            }
+            if (Plugin.RegionExtensionManager.DefineRegion(_player, region))
             {
                 _player.SendSuccessMessage("Set region " + _regionName);
             }
-            
         }
     }
 }
