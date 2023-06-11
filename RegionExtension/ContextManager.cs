8d22880ad12a05c958d37b1d747a234d16d26686
Rekman <rekman.heln@gmail.com>
2023-06-11 10:58:59 +0300

    Context specifier in help

--- RegionExtension/ContextManager.cs
+++ RegionExtension/ContextManager.cs
@@ -36,7 +36,7 @@ namespace RegionExtension
 
         private void Help(CommandArgs args)
         {
-            var strings = Contexts.Select(c => "{0} - {1}".SFormat(string.Join('/', c.Names), c.Description));
+            var strings = Contexts.Select(c => "{1} - {2}".SFormat(string.Join('/', c.Names), c.Description));
             int pageNumberCon = 1;
             if (args.Parameters.Count > 0)
             {
@@ -53,6 +53,8 @@ namespace RegionExtension
                   FooterFormat = "Type {0}/context {{0}} for more contexts.".SFormat(TShock.Config.Settings.CommandSpecifier)
               }
             );
+
+            args.Player.SendInfoMessage($"Specifier: {Plugin.Config.ContextSpecifier}");
 
         }
 
