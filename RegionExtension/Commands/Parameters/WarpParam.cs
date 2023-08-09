90cf95452727459aa0809050d5e5b1f70de37f0f
Rekman <rekman.heln@gmail.com>
2023-08-09 18:16:45 +0300

    Teleport triggers

---
+++ RegionExtension/Commands/Parameters/WarpParam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace RegionExtension.Commands.Parameters
+{
+    public class WarpParam : CommandParam<Warp>
+    {
+        public WarpParam(string name, string description, bool optional = false, Warp defaultValue = null) :
+            base(name, description, optional, defaultValue)
+        {
+        }
+
+        public override bool TrySetValue(string str, CommandArgsExtension args = null)
+        {
+            var warp = TShock.Warps.Warps.Where(w => w.Name.ToLower().Equals(str.ToLower()));
+            if (warp == null || warp.Count() < 1)
+            {
+                args?.Player.SendInfoMessage("Failed found warp '{0}'!".SFormat(str));
+                return false;
+            }
+            if (warp.Count() > 1)
+            {
+                args.Player.SendInfoMessage("Founded warps: ");
+                args.Player.SendInfoMessage(string.Join(", ", warp.Select(warp => warp.Name)));
+                return false;
+            }
+            _value = warp.First();
+            return true;
+        }
+    }
+}
---
+++ RegionExtension/RegionTriggers/Actions/TeleportToWarp.cs
@@ -0,0 +1,48 @@
+﻿using RegionExtension.Commands.Parameters;
+using RegionExtension.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI.DB;
+
+namespace RegionExtension.RegionTriggers.Actions
+{
+    internal class TeleportToWarp : ITriggerAction
+    {
+
+        private const int _pushDistance = 5;
+        public string Name => "warp";
+        public string Description => "Teleports player to the warp.";
+
+        public static ActionFormer Former { get; } = new ActionFormer(new[] { "warp" }, "Teleports player to the warp.",
+                                                                      new ICommandParam[]
+                                                                      {
+                                                                          new WarpParam("warp", "warp for tp.", false, null)
+                                                                      },
+                                                                      (param, args) => CreateTriggerAction(param, args),
+                                                                      s => new TeleportToWarp(s))
+        { Permission = Permissions.TriggerPush };
+        string _warpName;
+
+        public TeleportToWarp(string warpname)
+        {
+            _warpName = warpname;
+        }
+
+        public static ITriggerAction CreateTriggerAction(ICommandParam[] param, CommandArgsExtension args)
+        {
+            var warpName = ((Warp)param[0].Value).Name;
+            return new TeleportToWarp(warpName);
+        }
+
+        public void Execute(TriggerActionArgs args)
+        {
+            args.Player.Teleport((float)_x.Count(args.Player, args.Region) * 16, (float)_y.Count(args.Player, args.Region) * 16, 0);
+        }
+
+        public string GetArgsString() =>
+            string.Join(" ", _warpName);
+    }
+}
